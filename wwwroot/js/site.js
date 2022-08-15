8850ce7971bbce9740d2c68a4a5e15e59b072476
travis.hendricks <travis@ndrix.com>
2022-08-15 13:37:34 -0600

    added byId shorthand Javascript functions

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,9 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+
+// document.getElementById shorthand function
+// ex: byId("div-id").classList.add("new-class");
+var byId = function( id ) { return document.getElementById( id ); };
+// document.querySelector shorthand function
+var bySelector = function( selector ) { return document.querySelector( selector ); };
+// ex: bySelector("div-class").classList.add("new-class");
