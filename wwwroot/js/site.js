fd26c4f6955c0f3a2c84a8405b6679f0b8793965
travis.hendricks <travis@ndrix.com>
2022-08-19 13:10:09 -0600

    frontend is calling our API proxy and displaying raw JSON data

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -7,3 +7,26 @@ var byId = function( id ) { return document.getElementById( id ); };
 // document.querySelector shorthand function
 var bySelector = function( selector ) { return document.querySelector( selector ); };
 // ex: bySelector("div-class").classList.add("new-class");
+
+
+byId("search-button").addEventListener('click', function(){
+    const searchTerm = byId("search-box").value;
+    console.log("searchTerm", searchTerm);
+    searchByTerm(searchTerm);
+    console.log("called searchByTerm()", "true");
+});
+
+function searchByTerm(searchTerm) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            byId("search-results").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "https://localhost:7186/api/country/filter/" + searchTerm, true);
+    xhttp.send();
+}
+
+function buildResults(results) {
+    
+}
